41ce6e9180c8d0b2eaa37be02fa37c6adad5be2c
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-09-02 12:58:08 +0200

    Create main.php

---
+++ docs/pacchetti/main.php
@@ -0,0 +1,11 @@
+[Home Page](https://dev.hpbdev.cf) **>** [docs](https://dev.hpbdev.cf/docs/base) **>** [Pacchetti](https://dev.hpbdev.cf/docs/pacchetti/index) **>** Main<br>
+
+# Pacchetti di HPB -> `Main`
+
+**Classe**: Main<br>
+**Pacchetto**: `hpb\Main`<br>
+**Integrato dalla**: `v1.0 build2 ALPHA`<br>
+**Sviluppatore**: [FoxWorn3365](https://github.com/FoxWorn3365) <br>
+**Wiki**: [Wiki aggiornata](https://dev.hpbdev.cf/docs/Funzioni) <br>
+***
+La classe `Main` è la classe principale di HPB. Contiene tutte le funzioni base
